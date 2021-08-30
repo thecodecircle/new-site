02686f78b12d698f920e6fd7a466c39c3ae0ce07
gnpashi <guynatan@gmail.com>
2021-08-30 09:59:42 +0300

    nav

--- frontend/javascript/controllers/nav_controller.js
+++ frontend/javascript/controllers/nav_controller.js
@@ -1,6 +1,33 @@
 import { Controller } from "stimulus"
 export default class extends Controller {
+  static targets = ['nav', 'menu', 'scrollTop']
   connect() {
-    // console.log("Hello, Stimulus!", this.element)
+    let observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              this.navTarget.classList.remove("show")
+              this.scrollTopTarget.classList.remove("show")
+            } else {
+              console.log('add');
+              this.navTarget.classList.add("show")
+              this.scrollTopTarget.classList.add("show")
+          }
+      })
+  }, {
+      root: null,
+      rootMargin: '0%',
+      threshold: 1
+  });
+  if (document.querySelector(".hero-right")) {
+
+    observer.observe(document.querySelector(".hero-right"));
+  }
+
+  let navHeight =  window.getComputedStyle(this.navTarget).height
+  console.log(navHeight);
+  document.documentElement.style.setProperty('--nav-height', navHeight);
+  }
+  toggle(){
+    this.menuTarget.classList.toggle("show")
   }
 }
