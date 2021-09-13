668c68854652a52146de84a5ca471671fd540646
gnpashi <guynatan@gmail.com>
2021-09-13 23:44:43 +0300

    copy in before

--- frontend/javascript/controllers/posts_controller.js
+++ frontend/javascript/controllers/posts_controller.js
@@ -1,16 +1,14 @@
 import { Controller } from "stimulus"
 export default class extends Controller {
-  static targets = ['toast']
+  static targets = ['']
     connect(){
     document.querySelectorAll("code").forEach(element => {
         element.addEventListener("click", event => {
-            // element.select()
-            // element.setSelectionRange(0, 99999)
             navigator.clipboard.writeText(element.innerText)
             
-            this.toastTarget.classList.add("show")
+            element.classList.add('copied')
             setTimeout(() => {
-                this.toastTarget.classList.remove("show")
+                element.classList.remove('copied')
                 
             }, 1000);
         })
